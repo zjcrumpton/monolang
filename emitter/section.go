ed15ac9b3dfe04799db8b12d73a4705531af1c7a
Zac Crumpton <c-zac.crumpton@charter.com>
2022-04-01 22:01:48 -0600

    struck gold

--- emitter/section.go
+++ emitter/section.go
@@ -3,6 +3,7 @@ package emitter
 type SectionCode byte
 type ByteCode []byte
 
+// Section Types
 const (
 	CUSTOM_SECTION     SectionCode = 0x00
 	TYPE_SECTION       SectionCode = 0x01 // contains function signatures
@@ -18,10 +19,26 @@ const (
 	DATA_SECTION       SectionCode = 0x0b
 )
 
+// Signature Types for TYPE_SECTION
+const (
+	FUNC   = 0x60
+	INT_32 = 0x7f
+)
+
+// External Encoding
+const (
+	EXT_FUNC = 0x00
+)
+
 type Section struct {
 	ID      byte   // identifies the type of section
-	Size    uint32 // identifies how many bytes the section will contain
+	Size    int32  // identifies how many bytes the section will contain
 	Content []byte // the relevant section data (should be the same length as size)
+}
+
+type FuncSig struct {
+	Params  []byte
+	Results []byte
 }
 
 func (s *Section) ToByteCode() ByteCode {
@@ -34,9 +51,60 @@ func (s *Section) AddContent(c []byte) {
 	s.Content = append(s.Content, c...)
 }
 
-func NewSection(id SectionCode, s uint32) Section {
+func (s *Section) AddFuncSignatures(sigs []FuncSig) {
+	f := []byte{byte(len(sigs))}
+
+	for _, sig := range sigs {
+		f = append(f, FUNC)
+
+		numOfParams := len(sig.Params)
+		numOfResults := len(sig.Results)
+		f = append(f, byte(numOfParams))
+		f = append(f, sig.Params...)
+		f = append(f, byte(numOfResults))
+		f = append(f, sig.Results...)
+		f = append(f)
+	}
+
+	s.AddContent(f)
+	s.Size = int32(len(s.Content))
+}
+
+func (s *Section) AddFuncIndex(i int32) {
+	s.Size = 2
+	s.AddContent([]byte{0x01, byte(i)})
+}
+
+func (s *Section) AddImports(moduleName, exportName string, sigIndex int32) {
+	nameLen := len(moduleName)
+	exportLen := len(exportName)
+
+	s.AddContent([]byte{byte(nameLen)})
+	s.AddContent([]byte(moduleName))
+	s.AddContent([]byte{byte(exportLen)})
+	s.AddContent([]byte(exportName))
+	s.AddContent([]byte{EXT_FUNC, byte(sigIndex)})
+	s.Size = int32(len(moduleName) + len(exportName))
+}
+
+func (s *Section) AddExports(moduleNames []string) {
+	var c []byte
+	numOfExports := len(moduleNames)
+
+	c = append(c, byte(numOfExports))
+	for i, name := range moduleNames {
+		c = append(c, byte(len(name)))
+		c = append(c, []byte(name)...)
+		c = append(c, EXT_FUNC)
+		c = append(c, byte(i))
+	}
+
+	s.AddContent(c)
+	s.Size = int32(len(c))
+}
+
+func NewSection(id SectionCode) Section {
 	return Section{
-		ID:   byte(id),
-		Size: s,
+		ID: byte(id),
 	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"monolang/emitter"
+	"monolang/opcodes"
 	"os"
 )
 
@@ -29,7 +31,43 @@ func main() {
 	defer f.Close()
 
 	m := emitter.NewModule()
+
+	// TYPE SECTION
+	typeSection := emitter.NewSection(emitter.TYPE_SECTION)
+	typeSection.AddFuncSignatures([]emitter.FuncSig{
+		{
+			Results: []byte{emitter.INT_32},
+		},
+	})
+	m.AddSection(typeSection)
+
+	// FUNC SECTION
+	funcSection := emitter.NewSection(emitter.FUNC_SECTION)
+	funcSection.AddFuncIndex(0)
+	m.AddSection(funcSection)
+
+	// EXPORT SECTION
+	exportSection := emitter.NewSection(emitter.EXPORT_SECTION)
+	exportSection.AddExports([]string{"main"})
+	m.AddSection(exportSection)
+
+	// CODE SECTION
+	codeSection := emitter.NewSection(emitter.CODE_SECTION)
+	codeSection.Size = 0x07
+	codeSection.AddContent([]byte{0x01, 0x05, 0x00, opcodes.I32Const, byte(63), 0x0f, 0x0b})
+	m.AddSection(codeSection)
+
+	// // IMPORT SECTION
+	// importSection := emitter.NewSection(emitter.IMPORT_SECTION)
+	// importSection.AddImports("console", "log", 0)
+	// m.AddSection(importSection)
+
+	// imp := emitter.NewSection(emitter.IMPORT_SECTION, 10)
+	// imp.AddContent([]byte{'c', 'o', 'n', 's', 'o', 'l', 'e', ' ', 'l', 'o', 'g'})
+	// m.AddSection(imp)
+
 	code := m.ToByteCode()
+	fmt.Println(code)
 
 	f.Write(code)
 }
